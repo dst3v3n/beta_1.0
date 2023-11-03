52f17b7216a971a5b2193efcdaf0e7edcc959adb
JosueTorres2108 <josuett2108@gmail.com>
2023-11-02 20:18:16 -0500

    Views Josue

---
+++ HumanTalentSena/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index (request):
+    return render(request , 'index.html')
+
+def registro (request):
+    return render(request , 'registro.html')
+
+def login(request):
+    return render(request , 'login.html')
