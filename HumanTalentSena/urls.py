ba0c4eec74605d1acd2797250773d13de6ad0020
dst3v3n <hablack10@gmail.com>
2023-11-02 21:39:39 -0500

    second commit urls and templates

--- HumanTalentSena/urls.py
+++ HumanTalentSena/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index , name="index"),
+    path('registro/', views.registro, name="registro"),
+    path('login/', views.login , name="login"),
 ]
