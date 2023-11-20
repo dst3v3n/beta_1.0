00065f722d692b80449996a625cdd0d3febedb43
sdaiz27 <sebastiandiazlache@gmail.com>
2023-11-20 14:10:14 -0500

    delete error

--- administradores/urls.py
+++ administradores/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin_index/', views.admin_index, name='admin_index'),
-    path('visualizar/' , views.visualizar , name='visualizar_usuarios '),
-    path('usuarios/' , include('usuarios.urls')),
     
     
 ]
