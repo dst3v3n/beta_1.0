8c36cd0144014682e156b4e51dee8f7c7ad91e73
JosueTorres2108 <josuett2108@gmail.com>
2023-11-22 09:46:06 -0500

    PRIMERA PARTE CRUD JOSUE

--- administradores/urls.py
+++ administradores/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin_index/', views.admin_index, name='admin_index'),
     path('visualizar_users/', views.visualizar_tablas, name='visualizar'),
     path('acceso/', views.verificacion , name="verificacion"),
+    path('editarusuario/<int:id_usuario>',views.editarUsuario,name="editar_usuario"),
+    path("actualizar_usuario/<int:id_usuario>",views.actualizarUsuario, name="actualizar_usuario"),
 ]
